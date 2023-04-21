705cff8b0f740247976b90ccd82ec7c00cab1387
Antonin Huaut <me@antoninhuaut.fr>
2023-04-21 20:01:52 +0200

    GlobalRunnable class to function

--- src/GlobalRunnable.ts
+++ src/GlobalRunnable.ts
@@ -3,52 +3,51 @@ import DiscordRequests from './discord/DiscordRequests.ts';
 import { Config, DiscordConfig, DiscordData } from './model/Config.ts';
 import TwitchRequest from './twitch/TwitchRequest.ts';
 import TwitchRunnable from './twitch/TwitchRunnable.ts';
-import Logger from './utils/Logger.ts';
+import * as Logger from './utils/Logger.ts';
 
-export default class GlobalRunnable {
-    private static MIN_CHECK_INTERVAL = 1000;
+const MIN_CHECK_INTERVAL = 1000;
+let checkIntervalMs: number;
+let timeoutId: number;
+let discordClients: DiscordClient[];
+let twitchRuns: TwitchRunnable[];
 
-    private readonly checkIntervalMs: number;
+export function startGlobalRunnable(config: Config) {
+    if (timeoutId) return;
 
-    private readonly discordClients: DiscordClient[];
-    private readonly twitchRuns: TwitchRunnable[];
+    discordClients = [];
+    twitchRuns = [];
+    checkIntervalMs = Math.max(config.twitch.checkIntervalMs, MIN_CHECK_INTERVAL);
 
-    public constructor(config: Config) {
-        this.discordClients = [];
-        this.twitchRuns = [];
-        this.checkIntervalMs = Math.max(config.twitch.checkIntervalMs, GlobalRunnable.MIN_CHECK_INTERVAL);
+    createTwitchRunnable(config);
+    createDiscordClients(config.discord);
 
-        this.createTwitchRunnable(config);
-        this.createDiscordClients(config.discord);
+    tick();
+}
 
-        this.tick();
-    }
+async function tick() {
+    Logger.info(`GlobalRunnable (${discordClients.length} discordClients - ${twitchRuns.length} twitchRuns) ticking`);
 
-    private async tick() {
-        Logger.info(`GlobalRunnable (${this.discordClients.length} discordClients - ${this.twitchRuns.length} twitchRuns) ticking`);
+    const promises: Promise<void>[] = [];
+    twitchRuns.forEach((twitchRun: TwitchRunnable) => promises.push(twitchRun.tick()));
+    await Promise.all(promises);
 
-        const promises: Promise<void>[] = [];
-        this.twitchRuns.forEach((twitchRun: TwitchRunnable) => promises.push(twitchRun.tick()));
-        await Promise.all(promises);
+    promises.length = 0;
+    discordClients.forEach((discordClient: DiscordClient) => promises.push(discordClient.tick()));
+    await Promise.all(promises);
 
-        promises.length = 0;
-        this.discordClients.forEach((discordClient: DiscordClient) => promises.push(discordClient.tick()));
-        await Promise.all(promises);
+    timeoutId = setTimeout(() => tick(), checkIntervalMs);
+}
 
-        globalThis.setTimeout(() => this.tick(), this.checkIntervalMs);
-    }
+function createTwitchRunnable(config: Config) {
+    const twitchRequest: TwitchRequest = new TwitchRequest(config.twitch);
 
-    private createTwitchRunnable(config: Config) {
-        const twitchRequest: TwitchRequest = new TwitchRequest(config.twitch);
+    const twitchChannelsName: Set<string> = new Set();
+    config.discord.discords.map((discord: DiscordData) => twitchChannelsName.add(discord.twitchChannelName));
 
-        const twitchChannelsName: Set<string> = new Set();
-        config.discord.discords.map((discord: DiscordData) => twitchChannelsName.add(discord.twitchChannelName));
+    twitchChannelsName.forEach((twitchUsername: string) => twitchRuns.push(new TwitchRunnable(twitchRequest, twitchUsername)));
+}
 
-        twitchChannelsName.forEach((twitchUsername: string) => this.twitchRuns.push(new TwitchRunnable(twitchRequest, twitchUsername)));
-    }
-
-    private createDiscordClients(discordConfig: DiscordConfig) {
-        const discordRequests: DiscordRequests = new DiscordRequests(discordConfig);
-        discordConfig.discords.forEach((discord: DiscordData) => this.discordClients.push(new DiscordClient(discordRequests, discord, this.checkIntervalMs)));
-    }
+function createDiscordClients(discordConfig: DiscordConfig) {
+    const discordRequests: DiscordRequests = new DiscordRequests(discordConfig);
+    discordConfig.discords.forEach((discord: DiscordData) => discordClients.push(new DiscordClient(discordRequests, discord, checkIntervalMs)));
 }
--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs_relativeTime';
 
 import config from '../config.ts';
-import GlobalRunnable from './GlobalRunnable.ts';
+import { startGlobalRunnable } from './GlobalRunnable.ts';
 import I18nManager from './utils/I18nManager.ts';
 
 dayjs.extend(relativeTime);
 
 await I18nManager.getInstance().load();
-new GlobalRunnable(config);
+startGlobalRunnable(config);
